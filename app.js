58ff25c07d41a70a4a66a67939cf9f8db8db8a98
Zamir Kanji <zamirkanji@comcast.net>
2021-11-14 13:03:45 -0800

    almost done, need to figure out how to properly reset counter

--- app.js
+++ app.js
@@ -1,105 +1,151 @@
-const beginGame = document.querySelector('.begin-game');
-const rockbtn = document.querySelector('.rock-btn');
-const paperbtn = document.querySelector('.paper-btn');
-const scissorsbtn = document.querySelector('.scissors-btn');
-const selection = document.querySelector('.selection-container');
-
-const showComp = document.querySelector('#comp-selection')
-
-const displayScores = document.querySelector('.display-scores');
-const scores = document.querySelector('.scores');
-
-const computerScore = document.querySelector('.computer-score');
-const playerScore = document.querySelector('.player-score');
-const playAgain = document.querySelector('.play-again-container');
-
+//ALL CONSTANTS USED THROUGHOUT GAME
 const ROCK = "Rock";
 const PAPER = "Paper";
 const SCISSORS = "Scissors";
 const ERROR = "Error!";
 
+
+//QUERYSELECTORS
+const beginGame = document.querySelector('.begin-game');
+const rockbtn = document.querySelector('.rock-btn');
+const paperbtn = document.querySelector('.paper-btn');
+const scissorsbtn = document.querySelector('.scissors-btn');
+
+const selection = document.querySelector('.selection-container');
+
+const showComp = document.querySelector('#comp-selection')
+
+const roundWinner = document.querySelector('.round-winner-text');
+const displayScores = document.querySelector('.display-scores');
+const scores = document.querySelector('.scores');
+const compScoreDisplay = document.querySelector('.comp-score-container');
+const playerScoreDisplay = document.querySelector('.player-score-container');
+
+const gameWinner = document.querySelector('#winner');
+
+
+const computerScore = document.querySelector('.computer-score');
+const playerScore = document.querySelector('.player-score');
+const playAgain = document.querySelector('.play-again-container');
+const playAgainBtn = document.querySelector('.play-again');
+
+
 //computer and player counts, starting at zero
 let computerCount = 0;
 let playerCount = 0;
 
-beginGame.addEventListener('click', displayGame);
 
-//when clicked, check if scores are already displayed, if so, return and exit out of function before running 
+//check if visibility is not already removed 
+//if not, return 
+//if so, run displayGame()
+function checkVis () {
+	if (!selection.classList.contains('vis') || !displayScores.classList.contains('vis')) {
+		return;
+	} else {
+		displayGame();
+		//displayGame will not work
+	}
+}
 
-// beginGame.addEventListener('click', (e) => {
-// 	if (e.classList.contains('computer-score')) {
-// 		return;
-// 	} else {
-// 		displayScores
-// 	}
-// })
+//event listener for button to start game ---> 
+beginGame.addEventListener('click', checkVis);
 
 //append scores to displayscore div 
 const displayCount = () => {
-	
-
 	computerScore.textContent = computerCount;
 	playerScore.textContent = playerCount;
-	scores.appendChild(computerScore);
-	scores.appendChild(playerScore);
-}
 
+	compScoreDisplay.appendChild(computerScore);
+	playerScoreDisplay.appendChild(playerScore);
+}
 
 //when begin game button is clicked, display buttons and scores 
 function displayGame (e) {
 	displayCount();
-	console.log(e);
 	selection.classList.remove('vis');
 	displayScores.classList.remove('vis')
 
+	//starts game ---> run startGame()
 	startGame();
 }
 
-//once game ends, winner is declared, 
-function playAgainBtn () {
-	playAgain.classList.remove('vis');
-}
-
+let clicked = false;
 // computerScore.textContent = '';
 // playerScore.textContent = '';
 
-const startGame = () => {
+const startGame = (e) => {
+	console.log(e);
 	rockbtn.addEventListener('click', () => {
-		let selection = computerPlay();
+		clicked = true;
+		let selection = computerPlay(); //returning r from randomized function 
 		// console.log(selection);
-		showComp.textContent = `Computer chose: ${selection}`;
+		showComp.textContent = `Computer chooses: ${selection}`;
 		if (selection == SCISSORS) {
-			playerScore.textContent = ++playerCount;
+			roundWinner.textContent = `+1 Rock smashes Scissors`;
+			playerScore.textContent = playerCount += 1;
 		} else if (selection == PAPER) {
-			computerScore.textContent = ++computerCount;
+			roundWinner.textContent = `-1 Paper covers Rock`;
+			computerScore.textContent = computerCount += 1;
 		} else {
-			
+			roundWinner.textContent = `You tied!`;
 		}
+		// console.log(computerCount, playerCount);
+		checkIfOver(computerCount, playerCount);
 	})
 	paperbtn.addEventListener('click', () => {
+		clicked = true;
 		let selection = computerPlay();
 		// console.log(selection);
 		showComp.textContent = `Computer chose: ${selection}`;
-		if (selection == SCISSORS) {
-			playerScore.textContent = ++playerCount;
-		} else if (selection == PAPER) {
-			computerScore.textContent = ++computerCount;
+		if (selection == ROCK) {
+			playerScore.textContent = playerCount += 1;
+			roundWinner.textContent = `+1 Paper covers Rock`;
+		} else if (selection == SCISSORS) {
+			computerScore.textContent = computerCount += 1;
+			roundWinner.textContent = `-1 Scissors cuts Paper`;
 		} else {
-
+			roundWinner.textContent = `You tied!`;
 		}
+		checkIfOver(computerCount, playerCount);
 	})
 	scissorsbtn.addEventListener('click', () => {
+		clicked = true;
 		let selection = computerPlay();
 		// console.log(selection);
 		showComp.textContent = `Computer chose: ${selection}`;
-		if (selection == SCISSORS) {
-			playerScore.textContent = ++playerCount;
-		} else if (selection == PAPER) {
-			computerScore.textContent = ++computerCount;
+		if (selection == PAPER) {
+			playerScore.textContent = playerCount += 1;
+			roundWinner.textContent = `+1 Scissors cuts Paper`;
+		} else if (selection == ROCK) {
+			computerScore.textContent = computerCount += 1;
+			roundWinner.textContent = `-1 Rock smashes Scissors`;
 		} else {
-
+			roundWinner.textContent = `You tied!`;
 		}
+		checkIfOver(computerCount, playerCount);
 	})
+
+	
+	
+}
+
+function checkIfOver (comp, player) {
+	if (comp == 5 || player == 5) {
+		console.log("test");
+		declareWinner(computerCount, playerCount);
+	} else {
+		return;
+	}
+}
+function declareWinner (comp, player) {
+	console.log(comp, player + "test");
+	if (comp > player) {
+		gameWinner.textContent = `Machines are taking over the world. Uh Oh.`;
+	} else {
+		gameWinner.textContent = `You beat the Machine. Congrats.`;
+	}
+
+	playAgainFunc();
 }
 
 //once game starts and player makes selection, then run this function and return result to comp selection DOM 
@@ -110,83 +156,38 @@ let computerPlay = () => {
 	let r = Math.floor(Math.random() * (max - min) + min);
 	switch (r) {
 		case 1:
-			r = ROCK;
+		r = ROCK;
 			break;
-		case 2:
-			r = PAPER;
+			case 2:
+		r = PAPER;
 			break;
-		case 3:
-			r = SCISSORS;
+			case 3:
+		r = SCISSORS;
 			break;
-		default:
-			console.log("OOPS. Something went wrong");
+			default:
+		console.log("OOPS. Something went wrong");
 	}
-	console.log(`Computer chooses: ${r}`);
+	// console.log(`Computer chooses: ${r}`);
 	return r;
 };
 
+//once game ends, winner is declared, 
+function playAgainFunc () {
+	selection.classList.add('vis');
+	displayScores.classList.add('vis')
+	playAgain.classList.remove('vis');
 
-//function takes in player and computers input and returns winner of each round. incrementing count of winner
-function playRound (comp, player) {
-	// comp;
-	// player;
+	playAgainBtn.addEventListener('click', () => {
+		resetGame();
+		checkVis();
+	})
+}
 
-	//check if inputs are tied
-	if (comp == player) {
-		return "You Tied!";
-	}
-	//if not, check if player has paper
-	else if (player == PAPER) {
-		if (comp == ROCK) {
-			console.log("You Win!");
-			playerCount++;
-		} else {
-			console.log("You lost!");
-			computerCount++;
-		}	
-	} 
-	//check if player has rock
-	else if (player == ROCK) {
-		if (comp == PAPER) {
-			console.log("You lost!");
-			computerCount++;
-		} else {
-			console.log("You win!");
-			playerCount++;
-		}
-	} 
-	//check if player has scissors
-	else if (player == SCISSORS) {
-		if (comp == PAPER){
-			console.log("You won!");
-			playerCount++;
-		} else {
-			console.log("You lost!");
-			computerCount++;
-		}
-	} else {
-		return ERROR;
-	}
-	return `player score: ${playerCount}, computer score: ${computerCount}, `;
-};
-
-// let comp = computerPlay();
-// console.log(`computer selection log: ${comp}`);
-// let player = playerSelection();
-// console.log(`player selection log: ${player}`);
-// let round = playRound(comp, player);
-// console.log(round);
-                                                                                                                                                                                                                                                                                                                                                                                    
-
-// function game (playRound){
-// 	for (let i = 0; i < 5; i++) {
-// 		console.log(playRound);
-// 		let result = playRound;
-// 		console.log(playerCount, computerCount, result, i);
-// 		//not properly calling function - not asking for input everytime
-// 	}
-// }
-
-	// return result
-// console.log(playerCount, computerCount);
-// console.log(`game results test: ${game(round)}`);
+function resetGame() {
+	computerCount = 0;
+	playerCount = 0;
+	displayCount();
+	roundWinner.textContent = '';
+	showComp.textContent = '';
+	gameWinner.textContent = '';
+}
